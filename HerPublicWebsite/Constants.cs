26a8481e377b9b3a28996835e93242387a344842
Dan Corder <dan.corder@softwire.com>
2023-01-18 14:05:30 +0000

    feat: remove SEA feedback form URLs

--- HerPublicWebsite/Constants.cs
+++ HerPublicWebsite/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string FEEDBACK_URL_DEFAULT = "https://forms.microsoft.com/r/1G63YXE4Qq";
-    public const string FEEDBACK_URL_ACCESSIBILITY_PAGE = "https://forms.microsoft.com/r/2h0jTEzeyq";
+    public const string FEEDBACK_URL_DEFAULT = "TODO: Add form URL here";
+    public const string FEEDBACK_URL_ACCESSIBILITY_PAGE = "TODO: Add form URL here";
     public const string SERVICE_NAME = "Check if you might qualify for the Home Upgrade Grant";
 }
