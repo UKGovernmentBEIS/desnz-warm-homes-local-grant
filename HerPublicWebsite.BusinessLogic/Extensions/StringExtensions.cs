3316b4541d76c1e3841608d53d01870dd9e4d778
Dan Corder <dan.corder@softwire.com>
2023-04-26 17:54:05 +0100

    feat: Add code to check eligible post codes

---
+++ HerPublicWebsite.BusinessLogic/Extensions/StringExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HerPublicWebsite.BusinessLogic.Extensions;
+
+public static class StringExtensions
+{
+    private static Regex[] validPostCodeRegexes = new[]
+    {
+        new Regex(@"^[A-Z]\d \d[A-Z][A-Z]$", RegexOptions.Compiled),          //   AN NAA
+        new Regex(@"^[A-Z]\d\d \d[A-Z][A-Z]$", RegexOptions.Compiled),        //  ANN NAA
+        new Regex(@"^[A-Z][A-Z]\d \d[A-Z][A-Z]$", RegexOptions.Compiled),     //  AAN NAA
+        new Regex(@"^[A-Z][A-Z]\d\d \d[A-Z][A-Z]$", RegexOptions.Compiled),   // AANN NAA
+        new Regex(@"^[A-Z]\d[A-Z] \d[A-Z][A-Z]$", RegexOptions.Compiled),     //  ANA NAA
+        new Regex(@"^[A-Z][A-Z]\d[A-Z] \d[A-Z][A-Z]$", RegexOptions.Compiled) // AANA NAA
+    };
+
+    private static Regex multipleSpacesRegex = new Regex(@"[ ]{2,}", RegexOptions.Compiled);
+
+    // Returns true if a string is in the correct format to be a postcode.
+    // This method does not check whether the actual postcode exists
+    public static bool IsValidUkPostcodeFormat(this string value)
+    {
+        var interimValue = NormaliseToUkPostcodeFormatInternal(value);
+        return validPostCodeRegexes.Any(r => r.IsMatch(interimValue));
+    }
+
+    // Normalises a postcode to upper case with correct spacing. Returns null if the string cannot be a UK postcode
+    public static string NormaliseToUkPostcodeFormat(this string value)
+    {
+        var interimValue = NormaliseToUkPostcodeFormatInternal(value);
+        return validPostCodeRegexes.Any(r => r.IsMatch(interimValue)) ? interimValue : null;
+    }
+
+    private static string NormaliseToUkPostcodeFormatInternal(string value)
+    {
+        // Remove leading and trailing whitespace
+        var interimValue = value.Trim();
+
+        interimValue = interimValue.ToUpperInvariant();
+        
+        // Replace multiple consecutive spaces with a single space
+        interimValue = multipleSpacesRegex.Replace(interimValue, " ");
+
+        // All postcodes end in a number and two letters (the inward postcode)
+        // Insert a space before the inward postcode if it's not already there.
+        if (interimValue.Length >= 4 && interimValue[^4] != ' ')
+        {
+            interimValue = interimValue.Substring(0, interimValue.Length - 3) + " " +
+                           interimValue.Substring(interimValue.Length - 3);
+        }
+
+        return interimValue;
+    }
+}
---
+++ HerPublicWebsite.UnitTests/BusinessLogic/Services/EligiblePostcodeServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using HerPublicWebsite.BusinessLogic.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Tests.BusinessLogic.Services;
+
+[TestFixture]
+public class EligiblePostcodeServiceTests
+{
+    private ILogger<EligiblePostcodeService> logger;
+    private EligiblePostcodeService underTest;
+
+    [SetUp]
+    public void Setup()
+    {
+        logger = new NullLogger<EligiblePostcodeService>();
+
+        underTest = new EligiblePostcodeService(logger);
+    }
+    
+    [TestCase("AL1 2AP")]
+    [TestCase("YO8 9SF")]
+    public void IsEligiblePostcode_CalledWithEligiblePostcode_ReturnsTrue(string postcode)
+    {
+        // Act
+        var result = underTest.IsEligiblePostcode(postcode);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Test]
+    public void IsEligiblePostcode_CalledWithIneligiblePostcode_ReturnsFalse()
+    {
+        // Act
+        var result = underTest.IsEligiblePostcode("NW5 1TL");
+
+        // Assert
+        result.Should().BeFalse();
+    }
+    
+    [Test]
+    public void IsEligiblePostcode_CalledWithInvalidPostcode_ReturnsFalse()
+    {
+        // Act
+        var result = underTest.IsEligiblePostcode("NOT_A_POSTCODE");
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
