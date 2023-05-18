11be432f88fd99664747b9f11ae0a1dbcfa630cd
Zachary Freed <zachary.freed@softwire.com>
2023-05-18 17:09:49 +0100

    refactor: fixed displaying http response content in error message

--- HerPublicWebsite.BusinessLogic/ExternalServices/Common/ApiException.cs
+++ HerPublicWebsite.BusinessLogic/ExternalServices/Common/ApiException.cs
@@ -5,9 +5,9 @@ namespace HerPublicWebsite.BusinessLogic.ExternalServices.Common
     public class ApiException : Exception
     {
         public HttpStatusCode StatusCode;
-        public HttpContent Content;
+        public string Content;
 
-        public ApiException(string message, HttpStatusCode statusCode, HttpContent content) : base(message)
+        public ApiException(string message, HttpStatusCode statusCode, string content) : base(message)
         {
             StatusCode = statusCode;
             Content = content;
--- HerPublicWebsite.BusinessLogic/ExternalServices/Common/HttpRequestHelper.cs
+++ HerPublicWebsite.BusinessLogic/ExternalServices/Common/HttpRequestHelper.cs
@@ -45,13 +45,14 @@ namespace HerPublicWebsite.BusinessLogic.ExternalServices.Common
 
         private static T ConvertResponseToObject<T>(HttpResponseMessage response)
         {
+            var bodyString = response.Content.ReadAsStringAsync().Result;
+
             if (!response.IsSuccessStatusCode)
             {
                 throw new ApiException($"Request to {response.RequestMessage?.RequestUri} failed. " +
-                                       $"Error message: {response.StatusCode}; {response.ReasonPhrase}; {response.Content}", response.StatusCode, response.Content);
+                                       $"Error message: {response.StatusCode}; {response.ReasonPhrase}; {bodyString}", response.StatusCode, bodyString);
             }
 
-            var bodyString = response.Content.ReadAsStringAsync().Result;
             return JsonConvert.DeserializeObject<T>(bodyString);
         }
     }
