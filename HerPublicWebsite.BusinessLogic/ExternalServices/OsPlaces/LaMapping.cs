78886f6907563809beea8d12529d376040874ae6
Tim Leach <timleach635@gmail.com>
2023-06-19 14:38:56 +0100

    feat: Account for custodian codes that map on to each other

---
+++ HerPublicWebsite.BusinessLogic/ExternalServices/OsPlaces/LaMapping.cs
@@ -0,0 +1,14 @@
+﻿namespace HerPublicWebsite.BusinessLogic.ExternalServices.OsPlaces;
+
+// Sometimes, local authorities merge and we need to map the old custodian codes
+public class LaMapping
+{
+    public IEnumerable<string> OldCustodianCodes { get; }
+    public string NewCustodianCode { get; }
+
+    public LaMapping(IEnumerable<string> oldCustodianCodes, string newCustodianCode)
+    {
+        OldCustodianCodes = oldCustodianCodes;
+        NewCustodianCode = newCustodianCode;
+    }
+}
