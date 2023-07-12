8857147b77a71953d2b832b23b516e508511ff1e
Dan Corder <dan.corder@softwire.com>
2023-07-12 15:16:30 +0100

    refactor: Change how income bands to display are selected

--- HerPublicWebsite/Models/Questionnaire/HouseholdIncomeViewModel.cs
+++ HerPublicWebsite/Models/Questionnaire/HouseholdIncomeViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using GovUkDesignSystem.Attributes.ValidationAttributes;
+using HerPublicWebsite.BusinessLogic.Models;
 using HerPublicWebsite.BusinessLogic.Models.Enums;
 
 namespace HerPublicWebsite.Models.Questionnaire
@@ -7,5 +9,10 @@ namespace HerPublicWebsite.Models.Questionnaire
     {
         [GovUkValidateRequired(ErrorMessageIfMissing = "Select if your household income is £31,000 or less, or more than £31,000")]
         public IncomeBand? IncomeBand { get; set; }
+        
+        public string CustodianCode { get; set; }
+
+        public IEnumerable<IncomeBand> IncomeBandOptions =>
+            LocalAuthorityData.LocalAuthorityDetailsByCustodianCode[CustodianCode].IncomeBandOptions;
     }
 }
