3bc858ba5be9284e10e0d0acb02065610706c85b
Dan Corder <dan.corder@softwire.com>
2023-07-13 12:24:34 +0100

    fix: Remove LA from anonymous data

---
+++ HerPublicWebsite.Data/Migrations/20230713104758_RemoveLaFromAnonymisedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HerPublicWebsite.Data.Migrations
+{
+    public partial class RemoveLaFromAnonymisedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustodianCode",
+                table: "AnonymisedReports");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustodianCode",
+                table: "AnonymisedReports",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
