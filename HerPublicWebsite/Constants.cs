8fda732a267e1c41e0d7fc40889108399f390370
Dan Corder <dan.corder@softwire.com>
2023-07-20 14:15:56 +0100

    fix: Use real GDS home page in production

--- HerPublicWebsite/Constants.cs
+++ HerPublicWebsite/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string FEEDBACK_URL_DEFAULT = "https://forms.office.com/e/GHshSHbVep";
     public const string SERVICE_NAME = "Check if you are eligible for an energy grant for a home with no gas boiler";
-    public const string SERVICE_URL = "TODO: BEISHER-504 Add production landing page here";
+    public const string SERVICE_URL = "www.gov.uk/apply-home-upgrade-grant";
 }
