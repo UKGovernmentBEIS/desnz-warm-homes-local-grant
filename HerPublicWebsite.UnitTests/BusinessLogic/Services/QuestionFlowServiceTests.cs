135d013856c979a62979d85d709dbd7b89f1a72b
Sam Collard <sam.collard@softwire.com>
2023-10-06 12:41:00 +0100

    HER-NONE: Mark all Pending LAs as NotTakingPart temporarily

--- HerPublicWebsite.UnitTests/BusinessLogic/Services/QuestionFlowServiceTests.cs
+++ HerPublicWebsite.UnitTests/BusinessLogic/Services/QuestionFlowServiceTests.cs
@@ -611,16 +611,16 @@ public class QuestionFlowServiceTests
                 entryPoint: QuestionFlowStep.Address
             ),
             QuestionFlowStep.CheckAnswers),
-        new(
-            "Address selection continues to household income if authority is correct but income band is invalid and was changing answer",
-            new Input(
-                QuestionFlowStep.SelectAddress,
-                epcRating: EpcRating.D,
-                entryPoint: QuestionFlowStep.Address,
-                incomeBand: IncomeBand.GreaterThan31000,
-                custodianCode: "505" // Cambridge has income bands based on £34,000
-            ),
-            QuestionFlowStep.HouseholdIncome),
+        // new(
+        //     "Address selection continues to household income if authority is correct but income band is invalid and was changing answer",
+        //     new Input(
+        //         QuestionFlowStep.SelectAddress,
+        //         epcRating: EpcRating.D,
+        //         entryPoint: QuestionFlowStep.Address,
+        //         incomeBand: IncomeBand.GreaterThan31000,
+        //         custodianCode: "505" // Cambridge has income bands based on £34,000
+        //     ),
+        //     QuestionFlowStep.HouseholdIncome),
         new(
             "Address selection continues to review EPC if EPC is high and was changing answer",
             new Input(
@@ -645,16 +645,16 @@ public class QuestionFlowServiceTests
                 entryPoint: QuestionFlowStep.Address
             ),
             QuestionFlowStep.CheckAnswers),
-        new(
-            "Review EPC continues to household income if authority is correct but income band is invalid and was changing answer",
-            new Input(
-                QuestionFlowStep.ReviewEpc,
-                epcDetailsAreCorrect: EpcConfirmation.Yes,
-                entryPoint: QuestionFlowStep.Address,
-                incomeBand: IncomeBand.GreaterThan31000,
-                custodianCode: "505" // Cambridge has income bands based on £34,000
-            ),
-            QuestionFlowStep.HouseholdIncome),
+        // new(
+        //     "Review EPC continues to household income if authority is correct but income band is invalid and was changing answer",
+        //     new Input(
+        //         QuestionFlowStep.ReviewEpc,
+        //         epcDetailsAreCorrect: EpcConfirmation.Yes,
+        //         entryPoint: QuestionFlowStep.Address,
+        //         incomeBand: IncomeBand.GreaterThan31000,
+        //         custodianCode: "505" // Cambridge has income bands based on £34,000
+        //     ),
+        //     QuestionFlowStep.HouseholdIncome),
         new(
             "Review EPC continues to check answers if EPC is correct and was changing answer",
             new Input(
@@ -685,16 +685,16 @@ public class QuestionFlowServiceTests
                 entryPoint: QuestionFlowStep.Address
             ),
             QuestionFlowStep.SelectLocalAuthority),
-        new(
-            "Confirm local authority continues to household income if authority is correct but income band is invalid and was changing answer",
-            new Input(
-                QuestionFlowStep.ConfirmLocalAuthority,
-                localAuthorityIsCorrect: true,
-                entryPoint: QuestionFlowStep.Address,
-                incomeBand: IncomeBand.GreaterThan31000,
-                custodianCode: "505" // Cambridge has income bands based on £34,000
-            ),
-            QuestionFlowStep.HouseholdIncome),
+        // new(
+        //     "Confirm local authority continues to household income if authority is correct but income band is invalid and was changing answer",
+        //     new Input(
+        //         QuestionFlowStep.ConfirmLocalAuthority,
+        //         localAuthorityIsCorrect: true,
+        //         entryPoint: QuestionFlowStep.Address,
+        //         incomeBand: IncomeBand.GreaterThan31000,
+        //         custodianCode: "505" // Cambridge has income bands based on £34,000
+        //     ),
+        //     QuestionFlowStep.HouseholdIncome),
         new(
             "Confirm local authority continues to check answers if authority is correct if was changing answer",
             new Input(
@@ -761,7 +761,7 @@ public class QuestionFlowServiceTests
             DateTime? epcExpiry = null,
             IncomeBand? incomeBand = IncomeBand.UnderOrEqualTo31000,
             bool localAuthorityIsCorrect = false,
-            string custodianCode = "3805", // Adur has income threshold of £31,000
+            string custodianCode = "3505", // Babergh has income threshold of £31,000
             QuestionFlowStep? entryPoint = null)
         {
             Page = page;
