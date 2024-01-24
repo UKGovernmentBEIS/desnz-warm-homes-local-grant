b9fef41cb3696206036ec91d363c97828184ab02
Calum Pinder <calum.pinder@softwire.com>
2024-01-24 12:40:53 +0000

    Add more test cases to WorkingDayHelperService tests

--- HerPublicWebsite.UnitTests/BusinessLogic/Services/WorkingDayHelperServiceTests.cs
+++ HerPublicWebsite.UnitTests/BusinessLogic/Services/WorkingDayHelperServiceTests.cs
@@ -23,19 +23,29 @@ public class WorkingDayHelperServiceTests
         HttpRequestHelper.handler = mockHttpHandler;
     }
     
-    [Test]
-    public async Task AddWorkingDaysToDateTime_WhenCalledOnADayFollowingABankHoliday_CorrectlyCalculatesANewDate()
+    [TestCase("2024-01-24", 0, "2024-01-24")] // Zero days change
+    [TestCase("2024-01-24", +2, "2024-01-26")] // Adding days
+    [TestCase("2024-01-24", -2, "2024-01-22")] // Subtracting days
+    [TestCase("2024-01-24", +10, "2024-02-07")] // Over weekends
+    [TestCase("2024-05-8", -5, "2024-04-30")] // Over bank holiday
+    [TestCase("2024-05-30", -20, "2024-04-30")] // Over multiple bank holidays
+    [TestCase("2024-05-12", -3, "2024-05-08")] // Starting during weekend
+    [TestCase("2024-05-06", -3, "2024-05-01")] // Starting during bank holiday
+    [TestCase("2024-05-15", -3, "2024-05-10")] // Ending  during weekend
+    [TestCase("2024-05-09", -3, "2024-05-03")] // Ending  during bank holiday
+    [TestCase("2024-05-24 00:05:00", -5, "2024-05-17")] // Starting after midnight
+
+    public async Task AddWorkingDaysToDateTime_WhenCalledOnADayFollowingABankHoliday_CorrectlyCalculatesANewDate(DateTime initialDateTime, int workingDaysToAdd, DateTime expectedResult)
     {
         // Arrange
-        var initialDateTime = new DateTime(2023, 03, 23);
         var bankHolidayResponse = await File.ReadAllTextAsync("MockHttpResponses/bank-holidays.json");
         mockHttpHandler.Expect("https://www.gov.uk/bank-holidays.json")
             .Respond("application/json", bankHolidayResponse
         );
         // Act
-        var newDateTime = await workingDayHelperService.AddWorkingDaysToDateTime(initialDateTime, -10);
+        var newDateTime = await workingDayHelperService.AddWorkingDaysToDateTime(initialDateTime, workingDaysToAdd);
 
         // Assert
-        newDateTime.Should().BeSameDateAs(new DateTime(2023, 03, 08));
+        newDateTime.Should().BeSameDateAs(expectedResult);
     }
 }
