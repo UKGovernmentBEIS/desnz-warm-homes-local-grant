947925b1cd8a916bb634798b9220336ce7bba675
CalPinSW <104021096+CalPinSW@users.noreply.github.com>
2024-01-30 15:57:52 +0000

    PC-550: Add the ability to add bespoke text to LAs on the outcome page (#229)
    * PC-551: Use Partials in Pending and Confirmation Pages. Add default Partials for existing copy. Map LA Custodian Codes to Partial locations in QuestionnaireController.
    
    * Update PartialViewPath helper function names in Questionnaire Controller.
    
    * Code cleanliness and readability fixes
    
    * Improve Partial Views folder structure for Local Authority Messages
    
    * Simplify Partial View Path helper functions
    
    * Remove Custom Broadland route from confirmation PartialViewPath switch
    
    * Update variables to partialViewPath instead of location, Update switch statement to match new directory structure.
    
    * Fix typo in switch statement path

--- HerPublicWebsite/Models/Questionnaire/PendingViewModel.cs
+++ HerPublicWebsite/Models/Questionnaire/PendingViewModel.cs
@@ -7,6 +7,8 @@ namespace HerPublicWebsite.Models.Questionnaire;
 public class PendingViewModel : QuestionFlowViewModel
 {
     public string LocalAuthorityName { get; set; }
+    
+    public string LocalAuthorityMessagePartialViewPath { get; set; }
 
     [EmailAddress(ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
     [GovUkValidateRequiredIf(ErrorMessageIfMissing = "Enter your email address", IsRequiredPropertyName = nameof(IsEmailAddressRequired))]
