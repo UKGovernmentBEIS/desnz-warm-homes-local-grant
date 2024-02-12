a3063d40e0d4d45755bc84a8f123cdef3a3c86fc
Glenn Clarke <glenn.clarke@softwire.com>
2024-02-12 16:09:48 +0000

    PC-775: Formatting & comment for a property

--- HerPublicWebsite/Models/Questionnaire/PendingViewModel.cs
+++ HerPublicWebsite/Models/Questionnaire/PendingViewModel.cs
@@ -16,7 +16,9 @@ public class PendingViewModel : QuestionFlowViewModel
     
     public string LocalAuthorityMessagePartialViewPath { get; set; }
 
+    //This property is here as the Compare validation attribute requires an attribute to allow for only true values to be accepted.
     public bool isTrue => true;
+    
     [ModelBinder(typeof(GovUkCheckboxBoolBinder))]
     [Compare("isTrue", ErrorMessage = "You must acknowledge that your application will not be processed until your local authority has signed up to use the service")]
     public bool UserAcknowledgesApplicationNotProcessedUntilLocalAuthorityLive { get; set; }
