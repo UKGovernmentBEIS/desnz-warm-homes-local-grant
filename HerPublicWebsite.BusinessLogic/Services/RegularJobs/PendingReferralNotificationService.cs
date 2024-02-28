1fb2644a911fbd2cd4fa1b4a82858c54116117c8
Samuel Young <samuel.young@softwire.com>
2024-02-28 13:57:20 +0000

    PC-696: register new service to send pending emails

---
+++ HerPublicWebsite.BusinessLogic/Services/RegularJobs/PendingReferralNotificationService.cs
@@ -0,0 +1,25 @@
+﻿using HerPublicWebsite.BusinessLogic.ExternalServices.EmailSending;
+
+namespace HerPublicWebsite.BusinessLogic.Services.RegularJobs;
+
+public interface IPendingReferralNotificationService
+{
+    public void SendPendingReferralNotifications();
+}
+
+public class PendingReferralNotificationService : IPendingReferralNotificationService
+{
+    private IEmailSender emailSender;
+    
+    public PendingReferralNotificationService(
+        IEmailSender emailSender
+    )
+    {
+        this.emailSender = emailSender;
+    }
+    
+    public void SendPendingReferralNotifications()
+    {
+        this.emailSender.SendPendingReferralReportEmail();
+    }
+}
