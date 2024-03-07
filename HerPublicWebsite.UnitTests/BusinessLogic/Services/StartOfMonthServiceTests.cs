7496e31b3353d1aefd9b4d49ab58fc111c0e1daa
Samuel Young <samuel.young@softwire.com>
2024-03-07 17:35:41 +0000

    PC-883 temp, begin tests for StartOfMonthServic

---
+++ HerPublicWebsite.UnitTests/BusinessLogic/Services/StartOfMonthServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HerPublicWebsite.BusinessLogic.Services.RegularJobs;
+using NUnit.Framework;
+
+namespace Tests.BusinessLogic.Services;
+
+public class StartOfMonthServiceTests
+{
+    [Test]
+    public void StartOfMonthService_WhenCalled_ReturnsAPreviousDate()
+    {
+        // arrange
+        var date = DateTime.Today;
+        var startOfMonthService = new StartOfMonthService(date);
+        
+        // act
+        var result = startOfMonthService.GetStartOfPreviousMonth();
+        
+        // assert
+        Assert.That(result <= date);
+    }
+}
