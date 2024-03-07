9006578df192d0184ca8c78957323788f55ae877
Glenn Clarke <glenn.clarke@softwire.com>
2024-03-07 18:46:04 +0000

    PC-883: Further StartOfMonthService tests

--- HerPublicWebsite.UnitTests/BusinessLogic/Services/StartOfMonthServiceTests.cs
+++ HerPublicWebsite.UnitTests/BusinessLogic/Services/StartOfMonthServiceTests.cs
@@ -6,8 +6,33 @@ namespace Tests.BusinessLogic.Services;
 
 public class StartOfMonthServiceTests
 {
+    public void StartOfMonthService_WhenCalledWithoutParameter_ReturnsAPreviousDate()
+    {
+        // arrange
+        var startOfMonthService = new StartOfMonthService();
+        
+        // act
+        var result = startOfMonthService.GetStartOfPreviousMonth();
+        
+        // assert
+        Assert.That(result <= DateTime.Today);
+    }
+    
     [Test]
-    public void StartOfMonthService_WhenCalled_ReturnsAPreviousDate()
+    public void StartOfMonthService_WhenCalledWithoutParameter_ReturnsFirstDayOfAMonth()
+    {
+        // arrange
+        var startOfMonthService = new StartOfMonthService();
+        
+        // act
+        var result = startOfMonthService.GetStartOfPreviousMonth();
+        
+        // assert
+        Assert.That(result.Day == 1);
+    }
+    
+    [Test]
+    public void StartOfMonthService_WhenCalledWithParameter_ReturnsAPreviousDate()
     {
         // arrange
         var date = DateTime.Today;
@@ -18,5 +43,21 @@ public class StartOfMonthServiceTests
         
         // assert
         Assert.That(result <= date);
+    }
+    
+    [TestCase("2024-10-12", "2024-9-1")]
+    [TestCase("2024-9-1", "2024-8-1")]
+    [TestCase("2024-2-29", "2024-1-1")] //Leap day tests
+    [TestCase("2024-3-29", "2024-2-1")] //Leap day tests
+    public void StartOfMonthService_WhenCalledWithParameter_ReturnsFirstOfPreviousMonth(DateTime testDate, DateTime assertDate)
+    {
+        // arrange
+        var startOfMonthService = new StartOfMonthService(testDate);
+        
+        // act
+        var result = startOfMonthService.GetStartOfPreviousMonth();
+        
+        // assert
+        Assert.That(result == assertDate);
     }
 }
