1ceb7541da5625ca6d77e6d889148f7e1fc1c880
Samuel Young <samuel.young@softwire.com>
2024-03-11 09:54:49 +0000

    PC-883: fixup PendingReferralFilterServiceTests
    improve case comments to be more descriptive
    
    improve test name to include function, not class
    
    fix // Arrange comment location
    
    assert cases using FLuentAssertions
    
    remove redundant parentheses
    
    improve nesting on custodian code extracting code
    
    remove DateTime = import
    
    return a build referral request rather than setting member
    
    define dates surrounding last month a bit more flexibly
    
    move startOfPrevious month (prior oneMonthAgoDate) to setup

--- HerPublicWebsite.UnitTests/BusinessLogic/Services/PendingReferralFilterServiceTests.cs
+++ HerPublicWebsite.UnitTests/BusinessLogic/Services/PendingReferralFilterServiceTests.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FluentAssertions.Extensions;
 using HerPublicWebsite.BusinessLogic.Models;
 using HerPublicWebsite.BusinessLogic.Services.RegularJobs;
 using Moq;
 using NUnit.Framework;
 using Tests.Builders;
 using Tests.Helpers;
-using DateTime = System.DateTime;
+using static HerPublicWebsite.BusinessLogic.Models.LocalAuthorityData;
 
 namespace Tests.BusinessLogic.Services;
 
@@ -13,79 +16,78 @@ public class PendingReferralFilterServiceTests
 {
     private Mock<IDateHelper> mockDateHelper;
     private PendingReferralFilterService pendingReferralFilterService;
-    private ReferralRequest inputReferralRequest;
+    private DateTime startOfPreviousMonth;
 
     [SetUp]
     public void Setup()
     {
+        startOfPreviousMonth = 1.February(2024);
         mockDateHelper = new Mock<IDateHelper>();
+        mockDateHelper.Setup(mdh => mdh.GetStartOfPreviousMonth()).Returns(startOfPreviousMonth);
+        
         pendingReferralFilterService = new PendingReferralFilterService(mockDateHelper.Object);
     }
 
-    [TestCase(false, true, true)] // case 2
-    [TestCase(true, false, false)] // case 1
-    [TestCase(true, false, true)] // case 1, 3
-    [TestCase(true, true, false)] // case 1
-    [TestCase(true, true, true)] // case 1
-    public void PendingReferralFilterService_WhenCalledWithEmailPendingReferral_IncludesInFilter(
-        bool localAuthorityIsNowPending,
-        bool localAuthorityWasPending,
-        bool referralWasSubmittedInTheLastMonth)
-    {
-        SetupReferralData(localAuthorityIsNowPending, localAuthorityWasPending, referralWasSubmittedInTheLastMonth);
-        var inputReferralRequests = new List<ReferralRequest>() { inputReferralRequest };
-        
-        // Act
-        var outputReferralRequests = pendingReferralFilterService
-            .FilterForPendingReferralReport(inputReferralRequests);
-        
-        // Assert
-        CollectionAssert.Contains(outputReferralRequests, inputReferralRequest);
-    }
-
-    [TestCase(false, false, false)]
-    [TestCase(false, false, true)]
-    [TestCase(false, true, false)]
-    public void PendingReferralFilterService_WhenCalledWithNotEmailPendingReferral_DoesNotIncludeInFilter(
-        bool localAuthorityIsNowPending,
-        bool localAuthorityWasPending,
-        bool referralWasSubmittedInTheLastMonth)
-    {
-        SetupReferralData(localAuthorityIsNowPending, localAuthorityWasPending, referralWasSubmittedInTheLastMonth);
-        var inputReferralRequests = new List<ReferralRequest>() { inputReferralRequest };
-        
-        // Act
-        var outputReferralRequests = pendingReferralFilterService
-            .FilterForPendingReferralReport(inputReferralRequests);
-        
-        // Assert
-        CollectionAssert.DoesNotContain(outputReferralRequests, inputReferralRequest);
-    }
-
-    private void SetupReferralData(
+    // if LA is now pending, include
+    [TestCase(true, false, false)]
+    [TestCase(true, false, true)]
+    [TestCase(true, true, false)]
+    [TestCase(true, true, true)]
+    // if LA is not now pending but referral was submitted in the last month to a then pending LA, include
+    [TestCase(false, true, true)]
+    public void FilterForPendingReferralReport_WhenCalledWithEmailPendingReferral_IncludesInFilter(
         bool localAuthorityIsNowPending,
         bool localAuthorityWasPending,
         bool referralWasSubmittedInTheLastMonth)
     {
         // Arrange
-        var custodianCode =
-            LocalAuthorityDataHelper.GetExampleCustodianCodeForStatus(
-                localAuthorityIsNowPending
-                    ? LocalAuthorityData.Hug2Status.Pending
-                    : LocalAuthorityData.Hug2Status.Live);
+        var inputReferralRequest = BuildReferralRequest(localAuthorityIsNowPending, localAuthorityWasPending, referralWasSubmittedInTheLastMonth);
+        var inputReferralRequests = new List<ReferralRequest> { inputReferralRequest };
+        
+        // Act
+        var outputReferralRequests = pendingReferralFilterService
+            .FilterForPendingReferralReport(inputReferralRequests);
+        
+        // Assert
+        outputReferralRequests.Should().Contain(inputReferralRequest);
+    }
 
-        var requestDate = new DateTime(
-            2024,
-            referralWasSubmittedInTheLastMonth ? 2 : 1, // as 'last month' is defined as 1st february
-            15);
-        inputReferralRequest = new ReferralRequestBuilder(10)
+    [TestCase(false, false, false)]
+    [TestCase(false, false, true)]
+    [TestCase(false, true, false)]
+    public void FilterForPendingReferralReport_WhenCalledWithNotEmailPendingReferral_DoesNotIncludeInFilter(
+        bool localAuthorityIsNowPending,
+        bool localAuthorityWasPending,
+        bool referralWasSubmittedInTheLastMonth)
+    {
+        // Arrange
+        var inputReferralRequest = BuildReferralRequest(localAuthorityIsNowPending, localAuthorityWasPending, referralWasSubmittedInTheLastMonth);
+        var inputReferralRequests = new List<ReferralRequest> { inputReferralRequest };
+        
+        // Act
+        var outputReferralRequests = pendingReferralFilterService
+            .FilterForPendingReferralReport(inputReferralRequests);
+        
+        // Assert
+        outputReferralRequests.Should().NotContain(inputReferralRequest);
+    }
+
+    private ReferralRequest BuildReferralRequest(
+        bool localAuthorityIsNowPending,
+        bool localAuthorityWasPending,
+        bool referralWasSubmittedInTheLastMonth)
+    {
+        var currentStatus = localAuthorityIsNowPending ? Hug2Status.Pending : Hug2Status.Live;
+        var custodianCode = LocalAuthorityDataHelper.GetExampleCustodianCodeForStatus(currentStatus);
+        
+        var requestDate = referralWasSubmittedInTheLastMonth
+            ? startOfPreviousMonth.Add(10.Days())
+            : startOfPreviousMonth.Subtract(10.Days());
+        
+        return new ReferralRequestBuilder(10)
             .WithCustodianCode(custodianCode)
             .WithWasSubmittedToPendingLocalAuthority(localAuthorityWasPending)
             .WithRequestDate(requestDate)
             .Build();
-
-        var oneMonthAgoDate = new DateTime(2024, 2, 1);
-        
-        mockDateHelper.Setup(mdh => mdh.GetStartOfPreviousMonth()).Returns(oneMonthAgoDate);
     }
 }
