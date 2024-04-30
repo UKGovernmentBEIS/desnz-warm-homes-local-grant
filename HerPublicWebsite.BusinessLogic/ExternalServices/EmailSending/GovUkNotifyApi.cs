baa66cb694bbff4bfc20fe3d1db01fe1571840a1
Glenn-Clarke <152421975+Glenn-Clarke@users.noreply.github.com>
2024-04-30 17:28:53 +0100

    PC-NONE: changed email

--- HerPublicWebsite.BusinessLogic/ExternalServices/EmailSending/GovUkNotifyApi.cs
+++ HerPublicWebsite.BusinessLogic/ExternalServices/EmailSending/GovUkNotifyApi.cs
@@ -117,7 +117,7 @@ namespace HerPublicWebsite.BusinessLogic.ExternalServices.EmailSending
             MemoryStream historicLocalAuthorityReferralRequestFollowUpFileData,
             MemoryStream historicConsortiumReferralRequestFollowUpFileData)
         {
-            const string recipientList = "samuel.young@softwire.com"; // TODO PC-975 revert back to loading config
+            const string recipientList = "glenn.clarke@softwire.com"; // TODO PC-975 revert back to loading config
             var template = govUkNotifyConfig.ComplianceReportTemplate;
             var personalisation = new Dictionary<string, dynamic>
             {
