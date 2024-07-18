e238f5e3c3912848306ca4f1c3e02a71216cd39b
Glenn Clarke <glenn.clarke@softwire.com>
2024-07-18 14:50:54 +0100

    PC-873: Add IsJourneyComplete to models and database

--- HerPublicWebsite.BusinessLogic/Models/Session.cs
+++ HerPublicWebsite.BusinessLogic/Models/Session.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using CsvHelper;
 
 namespace HerPublicWebsite.BusinessLogic.Models;
 
@@ -8,4 +9,6 @@ public class Session
     public int Id { get; set; }
 
     public DateTime Timestamp;
+
+    public bool IsJourneyComplete { get; set; }
 }
