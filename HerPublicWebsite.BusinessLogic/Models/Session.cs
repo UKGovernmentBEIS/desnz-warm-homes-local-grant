328fa96f5715bee66f3fcb8f7e48d2b1c0ec8325
Glenn Clarke <glenn.clarke@softwire.com>
2024-07-25 11:53:42 +0100

    PC-1024: Update comment in Sessions.cs

--- HerPublicWebsite.BusinessLogic/Models/Session.cs
+++ HerPublicWebsite.BusinessLogic/Models/Session.cs
@@ -10,7 +10,10 @@ public class Session
     public DateTime Timestamp;
 
     public bool IsJourneyComplete { get; set; }
-    
-    //IsEligible being Null means eligibility has not been determined
+
+    /* While IsEligible == null, the user's eligibility has not been determined
+     * IsEligible should therefore always be null while IsJourneyComplete is false
+     * If IsJourneyComplete is true, the user's LA is Not Participating/Not Taking Part, so eligibility was not calculated
+     */
     public bool? IsEligible { get; set; }
 }
