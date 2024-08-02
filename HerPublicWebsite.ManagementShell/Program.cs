b6e804a02c8e9c24970bcf1c2fed30f7676fd395
Samuel Young <samuel.young@softwire.com>
2024-08-02 12:16:17 +0100

    PC-1133: add a managementshell project

---
+++ HerPublicWebsite.ManagementShell/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HerPublicWebsite.ManagementShell;
+
+public static class Program {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+}
