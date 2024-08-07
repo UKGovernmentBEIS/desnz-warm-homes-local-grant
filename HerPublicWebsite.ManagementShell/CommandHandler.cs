f252e7cf161d3d6018b32535f2d3a321f85dda15
Samuel Young <samuel.young@softwire.com>
2024-08-07 10:23:51 +0100

    PC-1133: add better messaging for CLI usage

--- HerPublicWebsite.ManagementShell/CommandHandler.cs
+++ HerPublicWebsite.ManagementShell/CommandHandler.cs
@@ -20,14 +20,27 @@ public class CommandHandler
     {
         if (args.Length == 0)
         {
-            outputProvider.Output("need a number please");
+            outputProvider.Output("Missing number of referrals. Usage: 'GenerateReferrals <count>'.");
             return;
         }
 
         if (!int.TryParse(args[0], out var referralCount))
         {
-            outputProvider.Output("thats a string");
+            outputProvider.Output("Invalid number of referrals. Usage: 'GenerateReferrals <count>'.");
         }
+        
+        outputProvider.Output("!!!!!!!!!!!!!!!!!!!!!!");
+        outputProvider.Output($"You are about to generate {referralCount} fake referral requests and add them to the database.");
+        outputProvider.Output("This command should only be run on development and staging environments.");
+        outputProvider.Output("Double check that you are NOT running this command on a production environment.");
+        outputProvider.Output("!!!!!!!!!!!!!!!!!!!!!!");
+        var confirmation = outputProvider.Confirm("Would you like to continue? (Y/N)");
+
+        if (!confirmation)
+        {
+            outputProvider.Output("No referrals generated");
+            return;
+        };
 
         var highestReferralId = databaseOperation.GetHighestReferralId();
         var referralsToAdd = fakeReferralGenerator.GenerateFakeReferralRequests(referralCount, highestReferralId + 1);
