f1d0100c0b1679b63b6ef9abe8f097cc4fa3ea40
Glenn Clarke <glenn.clarke@softwire.com>
2024-09-27 10:00:09 +0100

    PC-1235: change gas boiler error & check answers wording

--- HerPublicWebsite/Models/Questionnaire/GasBoilerViewModel.cs
+++ HerPublicWebsite/Models/Questionnaire/GasBoilerViewModel.cs
@@ -5,7 +5,7 @@ namespace HerPublicWebsite.Models.Questionnaire
 {
     public class GasBoilerViewModel : QuestionFlowViewModel
     {
-        [GovUkValidateRequired(ErrorMessageIfMissing = "Select yes if you have a gas boiler, or no if you do not have a gas boiler")]
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Select yes if you have a mains gas boiler, or no if you do not have a mains gas boiler")]
         public HasGasBoiler? HasGasBoiler { get; set; }
     }
 }
