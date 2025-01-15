2d5fbf04c5d283e097d16e3d032991f89489e800
Glenn Clarke <glenn.clarke@softwire.com>
2025-01-15 14:45:10 +0000

    PC-1523: Updated text to new copy, and retrieves shared ownership label dynamically

--- WhlgPublicWebsite.BusinessLogic/Models/Enums/OwnershipStatus.cs
+++ WhlgPublicWebsite.BusinessLogic/Models/Enums/OwnershipStatus.cs
@@ -9,9 +9,9 @@ namespace WhlgPublicWebsite.BusinessLogic.Models.Enums
     {
         [GovUkRadioCheckboxLabelText(Text = "Yes, I own my property and live in it")]
         OwnerOccupancy,
-        [GovUkRadioCheckboxLabelText(Text = "No, I am a tenant or social housing tenant")]
-        PrivateTenancy,
-        [GovUkRadioCheckboxLabelText(Text = "I am the property owner but lease my property to one or more tenants")]
+        [GovUkRadioCheckboxLabelText(Text = "Yes, I own my property but lease my property to one or more tenants")]
         Landlord,
+        [GovUkRadioCheckboxLabelText(Text = "No, I am a tenant or social housing tenant")]
+        PrivateTenancy
     }
 }
