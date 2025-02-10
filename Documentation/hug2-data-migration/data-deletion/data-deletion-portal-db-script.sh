c88d0b25fbe57e97035291ef79a8caf4e5caf04e
Glenn Clarke <glenn.clarke@softwire.com>
2025-02-10 11:52:58 +0000

    PC-1561: Use transactions in scripts

--- Documentation/hug2-data-migration/data-deletion/data-deletion-portal-db-script.sh
+++ Documentation/hug2-data-migration/data-deletion/data-deletion-portal-db-script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Function to run psql commands
-run_psql_command() {
-    PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U mysqladm -d "$DB_NAME" -c "$1"
-}
-
 echo "Deleting data from Portal DB..."
 
-# Truncate tables
-run_psql_command "TRUNCATE TABLE \"AuditDownloads\", 
-                                  \"Consortia\", 
-                                  \"ConsortiumUser\", 
-                                  \"CsvFileDownloads\", 
-                                  \"LocalAuthorities\", 
-                                  \"LocalAuthorityUser\", 
-                                  \"Users\";"
+# Connect to PostgreSQL and run queries
+PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U mysqladm -d "$DB_NAME" << EOF
+BEGIN;
+
+TRUNCATE TABLE "AuditDownloads", 
+                "Consortia", 
+                "ConsortiumUser", 
+                "CsvFileDownloads", 
+                "LocalAuthorities", 
+                "LocalAuthorityUser", 
+                "Users";
+
+COMMIT;
+EOF
 
 echo "Data deletion complete."
