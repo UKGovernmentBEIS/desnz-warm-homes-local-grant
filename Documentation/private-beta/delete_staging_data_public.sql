9096dc9da5f1dd4ab996bdcc0ae8ff62020cc1cc
Samuel Young <samuel.young@softwire.com>
2025-02-26 16:56:12 +0000

    fixup! PC-1778: Add data deletion scripts and instructions
    strengthen readme disclaimer
    
    bring date to soon after private beta ends

--- Documentation/private-beta/delete_staging_data_public.sql
+++ Documentation/private-beta/delete_staging_data_public.sql
@@ -1,6 +1,6 @@
 DO LANGUAGE plpgsql $$
     BEGIN
-        IF NOW() > '2025-04-01' THEN
+        IF NOW() > '2025-03-18' THEN
             RAISE NOTICE 'This script should only be run during the WH:LG Private Beta Phase on the Staging Environment';
             RAISE NOTICE 'Exiting without making changes...';
             RETURN;
