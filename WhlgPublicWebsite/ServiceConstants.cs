3c277f07ac25efc3d2ed35438037848bcd460b0e
Glenn Clarke <glenn.clarke@softwire.com>
2025-03-21 15:50:33 +0000

    PC-1810: Add EDI Questionnaire URL

--- WhlgPublicWebsite/ServiceConstants.cs
+++ WhlgPublicWebsite/ServiceConstants.cs
@@ -5,5 +5,5 @@ public static class ServiceConstants
     public const string FEEDBACK_URL_DEFAULT = "https://forms.office.com/e/j6J0LdRZNC";
     public const string SERVICE_NAME = "Apply for the Warm Homes: Local Grant to improve your home";
     public const string SERVICE_URL = "https://www.gov.uk/apply-warm-homes-local-grant";
-    public const string EDI_QUESTIONNAIRE_URL = "";
+    public const string EDI_QUESTIONNAIRE_URL = "https://forms.office.com/e/z8ZSfPgj4v";
 }
