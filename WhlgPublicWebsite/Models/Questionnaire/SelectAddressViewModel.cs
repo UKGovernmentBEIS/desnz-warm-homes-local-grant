06ea17e1e6a38018fd7d6ef5cef7c3319b752adb
Samuel Young <samuel.young@softwire.com>
2025-05-08 15:16:05 +0100

    PC-1530: Format touched files

--- WhlgPublicWebsite/Models/Questionnaire/SelectAddressViewModel.cs
+++ WhlgPublicWebsite/Models/Questionnaire/SelectAddressViewModel.cs
@@ -10,11 +10,14 @@ namespace WhlgPublicWebsite.Models.Questionnaire;
 public class SelectAddressViewModel : QuestionFlowViewModel
 {
     public List<Address> Addresses { get; set; }
-    [GovUkValidateRequiredIf(IsRequiredPropertyName = nameof(IsMultipleAddresses), ErrorMessageIfMissing = "Select your address or click \"Address not listed\"")]
+
+    [GovUkValidateRequiredIf(IsRequiredPropertyName = nameof(IsMultipleAddresses),
+        ErrorMessageIfMissing = "Select your address or click \"Address not listed\"")]
     [ModelBinder(typeof(GovUkSelectBinder))] // ensure the placeholder is interpreted as null
     public string SelectedAddressIndex { get; set; }
 
-    [GovUkValidateRequiredIf(IsRequiredPropertyName = nameof(IsSingleAddress), ErrorMessageIfMissing = "Select yes if it is your address")]
+    [GovUkValidateRequiredIf(IsRequiredPropertyName = nameof(IsSingleAddress),
+        ErrorMessageIfMissing = "Select yes if it is your address")]
     public YesOrNo? IsAddressCorrect { get; set; }
 
     public bool IsSingleAddress { get; set; }
