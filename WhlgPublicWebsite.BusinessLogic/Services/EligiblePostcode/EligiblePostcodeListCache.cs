f7d83e345f98cc7a78d30af35287a9e9c32cab43
Bogdan Maftei <bogdan.maftei@softwire.com>
2026-03-23 12:38:31 +0200

    DESNZ-2173: Added support for IMD2025 in EligiblePostcodeService

--- WhlgPublicWebsite.BusinessLogic/Services/EligiblePostcode/EligiblePostcodeListCache.cs
+++ WhlgPublicWebsite.BusinessLogic/Services/EligiblePostcode/EligiblePostcodeListCache.cs
@@ -6,24 +6,39 @@ namespace WhlgPublicWebsite.BusinessLogic.Services.EligiblePostcode;
 
 public class EligiblePostcodeListCache
 {
-    private readonly List<string> eligiblePostcodes;
+    // TODO DESNZ-2197: Remove the references to the old file (including the file itself) once we have moved entirely to the IMD2025 postcodes.
 
-    public EligiblePostcodeListCache()
+    private readonly List<string> eligiblePostcodes;
+    private readonly List<string> eligiblePostcodesImd2025;
+    private readonly IEligiblePostcodeImdFileChecker imdChecker;
+
+    public EligiblePostcodeListCache(IEligiblePostcodeImdFileChecker imdChecker)
+    {
+        this.imdChecker = imdChecker;
+
+        var jsonContents = ReadPostcodesFromJsonFile("EligiblePostcodeData");
+        var jsonContentsImd2025 = ReadPostcodesFromJsonFile("EligiblePostcodeData_IMD2025");
+
+        eligiblePostcodes = JsonConvert.DeserializeObject<List<string>>(jsonContents);
+        eligiblePostcodesImd2025 = JsonConvert.DeserializeObject<List<string>>(jsonContentsImd2025);
+    }
+
+    public List<string> GetEligiblePostcodes()
+    {
+        return imdChecker.ShouldUseImd2025()
+            ? eligiblePostcodesImd2025
+            : eligiblePostcodes;
+    }
+
+    private static string ReadPostcodesFromJsonFile(string fileName)
     {
         var info = Assembly.GetExecutingAssembly().GetName();
         var name = info.Name;
         using var stream = Assembly
             .GetExecutingAssembly()
-            .GetManifestResourceStream($"{name}.Services.EligiblePostcode.EligiblePostcodeData.json")!;
+            .GetManifestResourceStream($"{name}.Services.EligiblePostcode.{fileName}.json")!;
 
         using var reader = new StreamReader(stream, Encoding.UTF8);
-        var jsonContents = reader.ReadToEnd();
-
-        eligiblePostcodes = JsonConvert.DeserializeObject<List<string>>(jsonContents);
-    }
-
-    public List<string> GetEligiblePostcodes()
-    {
-        return eligiblePostcodes;
+        return reader.ReadToEnd();
     }
 }
--- WhlgPublicWebsite.UnitTests/BusinessLogic/Services/EligiblePostcodeServiceTests.cs
+++ WhlgPublicWebsite.UnitTests/BusinessLogic/Services/EligiblePostcodeServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+using System;
+using FluentAssertions;
 using WhlgPublicWebsite.BusinessLogic.Services.EligiblePostcode;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -14,8 +15,8 @@ public class EligiblePostcodeServiceTests
     {
         logger = new NullLogger<EligiblePostcodeService>();
 
-        var eligiblePostcodeListCache = new EligiblePostcodeListCache();
-        underTest = new EligiblePostcodeService(eligiblePostcodeListCache, logger);
+        var cache = new EligiblePostcodeListCache(new EligiblePostcodeImdFileChecker());
+        underTest = new EligiblePostcodeService(cache, logger);
     }
 
     private ILogger<EligiblePostcodeService> logger;
@@ -50,5 +51,36 @@ public class EligiblePostcodeServiceTests
 
         // Assert
         result.Should().BeFalse();
+    }
+
+    // TODO DESNZ-2197: Remove this once we have moved entirely to the IMD2025 postcodes.
+    // BN99 9GA: only in the original file
+    // N8 7JL: only in the IMD2025 file
+    // YO24 3LY: in both files
+    [TestCase("BN99 9GA", true, Description = "Only in original file")]
+    [TestCase("N8 7JL", false, Description = "Only in IMD2025 file")]
+    [TestCase("YO24 3LY", true, Description = "In both files")]
+    public void IsEligiblePostcode_BeforeApril2026_UsesOriginalPostcodes(string postcode, bool expectedResult)
+    {
+        var service = CreateServiceWithDate(new DateTime(2026, 3, 31));
+
+        service.IsEligiblePostcode(postcode).Should().Be(expectedResult);
+    }
+
+    [TestCase("BN99 9GA", false, Description = "Only in original file")]
+    [TestCase("N8 7JL", true, Description = "Only in IMD2025 file")]
+    [TestCase("YO24 3LY", true, Description = "In both files")]
+    public void IsEligiblePostcode_OnOrAfterApril2026_UsesImd2025Postcodes(string postcode, bool expectedResult)
+    {
+        var service = CreateServiceWithDate(new DateTime(2026, 4, 1));
+
+        service.IsEligiblePostcode(postcode).Should().Be(expectedResult);
+    }
+
+    private EligiblePostcodeService CreateServiceWithDate(DateTime date)
+    {
+        var imdChecker = new EligiblePostcodeImdFileChecker(() => date);
+        var cache = new EligiblePostcodeListCache(imdChecker);
+        return new EligiblePostcodeService(cache, logger);
     }
 }
